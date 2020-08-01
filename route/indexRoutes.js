1f41f6fb4a1517e59004712b2dacac017c0a0741
Deltaus <dsun@ucdavis.edu>
2020-08-01 03:39:12 -0700

    update server side

--- route/indexRoutes.js
+++ route/indexRoutes.js
@@ -1,10 +1,11 @@
 const express           = require('express');
         router            = express.Router();
 
+        /*
 router.get('/', function (req, res) {
     res.send('test');
 });
-
+*/
 router.get('/test', function (req, res) {
     res.send('Congrats!');
 })
