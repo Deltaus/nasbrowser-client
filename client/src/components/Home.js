a1a7c3f8bb84483168cff2d1786982469c91b217
Deltaus <dsun@ucdavis.edu>
2020-08-01 03:22:29 -0700

    update

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,6 +3,7 @@ import React from "react";
 const Home = () => {
     return (
         <div className='ui container'>
+            <h3>Home Page</h3>
             <div className='ui primary button'>Photos</div>
             <div className='ui primary button'>Videos/Movies</div>
         </div>
